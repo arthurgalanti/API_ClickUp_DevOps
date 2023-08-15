02bb339766d324b6ea21f6598de8b60a8565883c
ArthurGalanti <arthur.galanti@hotmail.com>
2023-08-15 13:06:41 -0300

    Add Webhook configuration

---
+++ Configuration.cs
@@ -0,0 +1,9 @@
+namespace apiClickupDevops {
+    public static class Configuration {
+        public const int IntegradorClickupId = "ID_INTEGRADOR_CLICKUP";
+        public const string IntegradorDevopsId = "ID_INTEGRADOR_DEVOPS";
+        public const int MaxNestedTask = 4;
+        public const int MaxTimesToCheckParent = 4;
+        public const int TimeToWaitParent = 2500;
+    }
+}
