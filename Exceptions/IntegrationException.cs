15d0198720e82858343e894dfc11d986fb2e863e
ArthurGalanti <arthur.galanti@hotmail.com>
2023-08-18 13:07:23 -0300

    Add Integration Exception

---
+++ Exceptions/IntegrationException.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace apiClickupDevops.Exceptions {
+    public class IntegrationException : Exception {
+
+        public IntegrationException(string message) : base(message) {
+        }
+    }
+
+
+}
