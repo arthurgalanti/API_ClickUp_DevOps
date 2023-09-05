008d17818f163baeb90b5067994c1fac2a588f61
ArthurGalanti <arthur.galanti@hotmail.com>
2023-09-05 13:12:46 -0300

    Add ClickupConfiguration model

---
+++ Models/Clickup/ClickupConfiguration.cs
@@ -0,0 +1,7 @@
+namespace apiClickupDevops.Models.Clickup {
+    public class ClickupConfiguration {
+        public string Project { get; set; } = null!;
+        public string AreaPath { get; set; } = null!;
+        public string WorkItemType { get; set; } = null!;
+    }
+}
